49bc74bd3593fd266b19f65026c01a58cf561bc8
Layla <Layla2@MacBook-Air.admin.ad.cnm.edu>
2022-08-15 13:00:40 -0500

    still buggy but less so

--- HelloPHSensor/src/HelloPHSensor.cpp
+++ HelloPHSensor/src/HelloPHSensor.cpp
@@ -13,42 +13,96 @@
 
 void setup();
 void loop();
-int takePhReading();
+float takePhReading(int _sensorPin, int _ledpin);
+float averageArray(int *arr, int number);
 #line 8 "/Users/Layla2/Documents/IoT/Elevated-Fish-Tank/HelloPHSensor/src/HelloPHSensor.ino"
-const int pHPin =  A4;
-const int offset = 41; //will need to calculate later for my specfic sensor for 
-const int LED = D7;
+const int pHPin  =  A4;
+ //will need to calculate later for my specfic sensor for 
+const int LEDPIN = D7;
 const int sampleInterval = 20;
 const int printInterval = 800;  //not quite sure what this is for yet
 const int phArrayLength = 40;
 int phArray[phArrayLength];  //average the values of the reading for a more statistically accurate result
-int i = 0;
-
+float phVal; 
+//int i;
 void setup() {
-  pinMode(LED, OUTPUT);
+  pinMode(LEDPIN, OUTPUT);
   //pinMode(pHpin, INPUT);
   Serial.begin(9600);
-
 }
-
 void loop() {
-  // The core of your code will likely live here.
-
+  phVal = takePhReading(pHPin, LEDPIN);
+  Serial.printf("PH VAL: %.2f" ,phVal);
 }
 
-int takePhReading(){
+float takePhReading(int _sensorPin, int _ledpin){
+  const int offset = 41;
   static unsigned long samplingTime = millis();
   static unsigned long  printTime = millis();
   static float pHValue, voltage;
+  int i = 0;  //maybe make static 
   if(millis()- samplingTime > sampleInterval){  
      //this feels like a lot for one line of code
-     pHArray[i++] = analogRead(pHpin); 
-     if(phArrayLength == 40){
+     phArray[i++] = analogRead(_sensorPin); 
+     if(phArrayLength == 40){  
       i = 0;  //reset the index
-      voltage = averagearray()
+      voltage = averageArray(phArray, phArrayLength)*(5.0/1024); //conversion factor place elsewhere later?
+      pHValue = -19.185119 * voltage + offset;
+      samplingTime = millis();
      }
-
-
-
+     return pHValue; //for now
   }
+  if(millis()- printTime > printInterval){
+      Serial.printf("Voltage: %f \n" , voltage);
+      Serial.printf("PH value: %f \n" , pHValue);
+      digitalWrite(_ledpin, digitalRead(_ledpin) ^ 1);  //flashes the LED using a bitwise operator
+      printTime = millis();
+   }
+}
+
+float averageArray(int *arr, int number){
+float _avg;  //why does this use the data type double? i changed it to float.
+int i; 
+int _max, _min;  
+float amount = 0.00;  //long data type?
+if (number <= 0){
+  Serial.printf("Error in Number being returned \n");
+  return 0;
+}
+  if (number < 5){
+    for(i = 0; i < number , 0; i++){  //for cases where there are less than 3 elements in the array
+     amount += arr[i];
+    }
+     _avg = amount/ number;
+  return _avg;
+  }  
+  else {  //all of the following seems to be happening in the if statement 
+    //for when there are only two elements in the array?
+    if(arr[0] < arr[1]){ 
+    _min = arr[0]; 
+    _max = arr[1]; 
+    }  
+  else{
+    _min = arr[1];
+    _max = arr[0];  
+  }  //else
+  for(i = 2; i < number; i++){  //why does it need to start here?
+    if(arr[i] < _min){  //storing min and max in first two elements of the array for some reason
+      amount += _min;
+      _max = arr[i];
+    }  //if
+    else {
+      if(arr[i] > _max ){
+        //sum the amount with the minimum amount 
+        amount += _max;   //an element of array < current Minimum 
+        _max = arr[i];  //arr > max
+      }  //if
+      else{
+        amount += arr[i];  //when the array[i] is between min and max
+      }
+    } //else
+  } //for
+  _avg = amount/(number-2);
+} //if
+return _avg;
 }
--- HelloPHSensor/src/HelloPHSensor.ino
+++ HelloPHSensor/src/HelloPHSensor.ino
@@ -5,41 +5,48 @@
  * Date: 8/15/2022
  */
 
-const int pHPin =  A4;
+const int pHPin  =  A4;
  //will need to calculate later for my specfic sensor for 
-const int LED = D7;
+const int LEDPIN = D7;
 const int sampleInterval = 20;
 const int printInterval = 800;  //not quite sure what this is for yet
 const int phArrayLength = 40;
 int phArray[phArrayLength];  //average the values of the reading for a more statistically accurate result
-int i = 0;
-
+float phVal; 
+//int i;
 void setup() {
-  pinMode(LED, OUTPUT);
+  pinMode(LEDPIN, OUTPUT);
   //pinMode(pHpin, INPUT);
   Serial.begin(9600);
-
 }
-
 void loop() {
-  // The core of your code will likely live here.
-
+  phVal = takePhReading(pHPin, LEDPIN);
+  Serial.printf("PH VAL: %.2f" ,phVal);
 }
 
-int takePhReading(){
+float takePhReading(int _sensorPin, int _ledpin){
   const int offset = 41;
   static unsigned long samplingTime = millis();
   static unsigned long  printTime = millis();
   static float pHValue, voltage;
+  int i = 0;  //maybe make static 
   if(millis()- samplingTime > sampleInterval){  
      //this feels like a lot for one line of code
-     pHArray[i++] = analogRead(pHpin); 
-     if(phArrayLength == 40){
+     phArray[i++] = analogRead(_sensorPin); 
+     if(phArrayLength == 40){  
       i = 0;  //reset the index
       voltage = averageArray(phArray, phArrayLength)*(5.0/1024); //conversion factor place elsewhere later?
-      pHValue = -19.185119c* voltage + Offset;
+      pHValue = -19.185119 * voltage + offset;
       samplingTime = millis();
      }
+     return pHValue; //for now
+  }
+  if(millis()- printTime > printInterval){
+      Serial.printf("Voltage: %f \n" , voltage);
+      Serial.printf("PH value: %f \n" , pHValue);
+      digitalWrite(_ledpin, digitalRead(_ledpin) ^ 1);  //flashes the LED using a bitwise operator
+      printTime = millis();
+   }
 }
 
 float averageArray(int *arr, int number){
@@ -49,27 +56,31 @@ int _max, _min;
 float amount = 0.00;  //long data type?
 if (number <= 0){
   Serial.printf("Error in Number being returned \n");
-  for(i = 0; i , 0; i++){  //for cases where there are less than 3 elements in the array
-    amount += arr[i];
-    _avg = amount/ number;
-    return _avg;
+  return 0;
+}
+  if (number < 5){
+    for(i = 0; i < number , 0; i++){  //for cases where there are less than 3 elements in the array
+     amount += arr[i];
+    }
+     _avg = amount/ number;
+  return _avg;
   }  
   else {  //all of the following seems to be happening in the if statement 
     //for when there are only two elements in the array?
     if(arr[0] < arr[1]){ 
-    min = arr[0]; 
-    max = arr[1]; 
-    }  /
+    _min = arr[0]; 
+    _max = arr[1]; 
+    }  
   else{
-    min = arr[1];
-    max = arr[0];  
+    _min = arr[1];
+    _max = arr[0];  
   }  //else
-  for(i = 2; i < number; i++){ . //why does it need to start here?
+  for(i = 2; i < number; i++){  //why does it need to start here?
     if(arr[i] < _min){  //storing min and max in first two elements of the array for some reason
       amount += _min;
       _max = arr[i];
     }  //if
-    else{
+    else {
       if(arr[i] > _max ){
         //sum the amount with the minimum amount 
         amount += _max;   //an element of array < current Minimum 
@@ -78,10 +89,9 @@ if (number <= 0){
       else{
         amount += arr[i];  //when the array[i] is between min and max
       }
-      } //else
-    } //for
-  } //if
-  _avg = _amount/(number-2);
-}  //if
+    } //else
+  } //for
+  _avg = amount/(number-2);
+} //if
 return _avg;
 }
