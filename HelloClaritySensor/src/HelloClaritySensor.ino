2342e5a4d77c8724710b48a7cc9218133a7a62b3
Layla <Layla2@MacBook-Air.local>
2022-08-17 01:06:35 -0500

    this is betterish

--- HelloClaritySensor/src/HelloClaritySensor.ino
+++ HelloClaritySensor/src/HelloClaritySensor.ino
@@ -20,7 +20,6 @@ void setup() {
     Serial.begin(9600);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
  // currentTime = millis();
   //if((currentTime - lastTime) > 2000){
@@ -30,37 +29,25 @@ void loop() {
  // }
   laserTimer.startTimer(5000);
    Serial.printf("Starting Time \n");
-  
-  turnLaserOn(LASERPIN);
+
   getTurbidity(TURPIN);
-  }
-void turnLaserOn(int _digitalPin){  
-  //perhaps make a bool function    //laserTimer.startTimer(5000); 
-    //Serial.printf("Starting Time \n");
-    if(!laserTimer.isTimerReady()){
-    digitalWrite(_digitalPin, HIGH);
-    Serial.printf("Laser on \n");
-    }
-    else if(laserTimer.isTimerReady()){
-      digitalWrite(_digitalPin, LOW);
-      Serial.printf("Laser off \n");
-    }
+    
 }
-  float getTurbidity(int _sensorPin) {  
+  float Turbidity(int _sensorPin) {  
     //maybe make average its own function and tobidity another one
-  float turbidity;
-  float _avg;
-  int vAnalogRead, i;
-  int samplingTime; 
-  int _interval = 5000;
+  static float turbidity;
+  static float samplingTime; 
+  float _median;
+  int vAnalogRead[], i; 
+  int _interval = 30000;  //for testing purposes
   //turnLaserOn(LASERPIN); //just an idea
    if((millis()- samplingTime) > _interval){
     for (i=0; i< 800; i++){
-      vAnalogRead = vAnalogRead + analogRead(_sensorPin);
+      vAnalogRead[i] = analogRead(_sensorPin);
       delayMicroseconds(100);
     }
-     _avg = vAnalogRead/800.00;
-     Serial.printf("Avg: %.2f \n" ,_avg);
+      median[i] = vAnalogRead/800.00;
+      Serial.printf("Avg: %.2f \n" ,_avg);
       turbidity = -1185551.78*pow((1/_avg), 2) + 6874.09 * (1/_avg) + 0.091;  
       //quadratic regression for this specific sensor
       //linear regression for data 2 is lowest cloudiness I could find
@@ -69,3 +56,36 @@ void turnLaserOn(int _digitalPin){
    }
   return turbidity;
   }
+
+  //float turpidity();  //rewriting 
+      //float tubidity;
+
+
+int getMedian(int array1[], int  arrayLen) {  //treat like pointer?
+  //tabulates the reading in array and divides by total readings to smooth out variabilities
+  int array1Tab[arrayLen];  
+  int i, j, arrayTemp;
+      //array1tab identifies the tabulated sum of readings stored
+  for (byte i = 0; i < arrayLength; i++){
+    //array Len  is the number of times being sampled in my case 800
+    array1tab[i] = array[i];  //making copy of array for some reason
+  }  //does this tab go here?
+  for (j = 0; j < arrayLength - 1; j++){ //take largest number and move it to end of the array //tells you how many elements in the array from the end have been ordered
+    for (i = 0; i < arrayLength - j - 1; i++) {  //for each index go through previous indexes
+      if (array1Tab[i] > array1Tab[i + 1]){   
+        // if some number is greater than the next
+        arrayTemp = array1Tab[i];  
+        array1Tab[i] = array1Tab[i + 1];  //swap values in i and i+i places of array
+        array1Tab[i + 1] = arrayTemp;   
+      }
+    Serial.printf("Arr tab: %i \n Array Temp: %i \n" , array1[i], arrayTemp);
+    }
+  }
+  if ((arrayLen & 1) > 0){  //checks if even/odd  //if array Len and 1 are greater than 0?
+    arrayTemp = array1Tab[(arrayLen - 1) / 2]; //subtract 1 due to the number of array starting at 0, and check if it in 
+  }
+  else{  //i added these brackets 
+    arrayTemp = (array1Tab[arrayLen/ 2] + array1Tab[arrayLen / 2 - 1]) / 2;
+  }
+   return arrayTemp;
+}
