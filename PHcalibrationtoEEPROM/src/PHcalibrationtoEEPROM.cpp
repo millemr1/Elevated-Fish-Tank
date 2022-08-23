ac3bcba313ff989b54a870596cf4a2a678e5f8d3
Layla <Layla2@MacBook-Air.admin.ad.cnm.edu>
2022-08-23 18:00:31 -0500

    almost working

--- PHcalibrationtoEEPROM/src/PHcalibrationtoEEPROM.cpp
+++ PHcalibrationtoEEPROM/src/PHcalibrationtoEEPROM.cpp
@@ -13,39 +13,73 @@
 
 
 void setup();
-void floattoEEPROM(double _float1, u_int8_t _startingAddress);
+void loop();
+bool float2eeprom(float value, uint16_t addr);
+float eeprom2float(uint16_t _addr);
 #line 9 "/Users/Layla2/Documents/IoT/Elevated-Fish-Tank/PHcalibrationtoEEPROM/src/PHcalibrationtoEEPROM.ino"
-double slope = -88.31;
-double offset = 2935.00;
+float slope = -88.31;
+float offset = 2935.00;
+bool s, o;
+float slo, off; 
 
 void setup() {
-  floattoEEPROM(slope, 0xA1);
-  floattoEEPROM(offset, 0xB1);
+  s = float2eeprom(slope, 0xA1);
+  o = float2eeprom(offset, 0xB1);
+  Serial.printf("Slope stored %i \n , OffsetStored %i" , s, o);
+  slo = eeprom2float(0xA1);
+  off = eeprom2float(0XB1);
+  Serial.printf("Slope:  %.2f\n , Offset: %.2f\n" , slo , off);
+}
+void loop() {
+
+  s = float2eeprom(slope, 0xA1);
+  o = float2eeprom(offset, 0xB1);
+  Serial.printf("Slope stored %i \n , OffsetStored %i" , s, o);
+  slo = eeprom2float(0xA1);
+  off = eeprom2float(0XB1);
+  Serial.printf("Slope:  %.2f\n , Offset: %.2f\n" , slo , off);
 }
 
-// void loop() {
-
-// }
 //maybe make a bool function
-void floattoEEPROM(double _float1, u_int8_t _startingAddress){  //from least to greatest byte values
-  int byte_0, byte_1, byte_2, byte_3;
-  int _0, _1, _2, _3;
+bool float2eeprom(float value, uint16_t addr){  //from least to greatest byte values
+  byte byte0, byte1, byte2, byte3;
+  int32_t value2save;
+  bool matched;
 
-    byte_0 = (_float1 && 0xFF); //LSB //maybe make a bool function
-    byte_1 = ((_float1 >> 8) && 0XFF);
-    byte_2 = ((_float1 >> 16) && 0xFF);
-    byte_3 = ((_float1 >> 24)&& 0xFF);  //MSB
+  value2save =  value*1000;  //convert to int to cicumvent data type issues
 
-    EEPROM.write(_startingAddress, byte_0);  //storing from least address to greatest
-    EEPROM.write(_startingAddress + 1, byte_1);
-    EEPROM.write(_startingAddress + 2, byte_2);
-    EEPROM.write(_startingAddress + 3, byte_3);
+    byte0 = (value2save & 0xFF); //LSB //maybe make a bool function
+    byte1 = ((value2save >> 8) & 0XFF);
+    byte2 = ((value2save >> 16) & 0xFF);
+    byte3 = ((value2save >> 24) & 0xFF);  //MSB
 
-  _0 = EEPROM.read(_startingAddress);  // values in specfic memory location
-  _1 = EEPROM.read(_startingAddress+1);
-  _2 = EEPROM.read(_startingAddress+2);
-  _3 = EEPROM.read(_startingAddress+3);
-
-Serial.printf("Address 0: %X \n Address 1: %X \n, Address 2: %X \n Address 3: %X \n", _0, _1, _2, _3);
+    EEPROM.write(addr, byte0);  //storing from least address to greatest
+    EEPROM.write(addr + 1, byte1);
+    EEPROM.write(addr + 2, byte2);
+    EEPROM.write(addr + 3, byte3);
+    if(value = eeprom2float(addr)){ //just == or =
+      matched = true;
+      Serial.printf("matched \n");
+    }
+    else{
+        matched =  false;
+        Serial.printf("not matched \n");
+    }
+    return matched;
 }
-//void EEPROMtoFloat(){}
+float eeprom2float(uint16_t _addr){ 
+    byte byte0, byte1, byte2, byte3; //ordered LSB TO MSB
+    int value;
+    float valueF;
+
+  byte0 = EEPROM.read(_addr);  // values in specfic memory location
+  byte1 = EEPROM.read(_addr+1);
+  byte2 = EEPROM.read(_addr+2);
+  byte3 = EEPROM.read(_addr+3);
+
+  value =  (byte3 << 24) | (byte2 << 16) | (byte1 << 8) | (byte0);
+  valueF = value/1000.0;
+  Serial.printf("Value %.2f \n " , valueF);
+
+  return valueF;
+}
--- PHcalibrationtoEEPROM/src/PHcalibrationtoEEPROM.ino
+++ PHcalibrationtoEEPROM/src/PHcalibrationtoEEPROM.ino
@@ -9,38 +9,43 @@
 float slope = -88.31;
 float offset = 2935.00;
 bool s, o;
-int store; 
-
+float slo, off; 
 
 void setup() {
-  s = floattoEEPROM(slope, 0xA1);
-  o = floattoEEPROM(offset, 0xB1);
-  Serial.printf('Slope stored %i \n , OffsetStored');
-  
-
+  s = float2eeprom(slope, 0xA1);
+  o = float2eeprom(offset, 0xB1);
+  Serial.printf("Slope stored %i \n , OffsetStored %i" , s, o);
+  slo = eeprom2float(0xA1);
+  off = eeprom2float(0XB1);
+  Serial.printf("Slope:  %.2f\n , Offset: %.2f\n" , slo , off);
 }
-// void loop() {
+void loop() {
 
-// }
+  s = float2eeprom(slope, 0xA1);
+  o = float2eeprom(offset, 0xB1);
+  Serial.printf("Slope stored %i \n , OffsetStored %i" , s, o);
+  slo = eeprom2float(0xA1);
+  off = eeprom2float(0XB1);
+  Serial.printf("Slope:  %.2f\n , Offset: %.2f\n" , slo , off);
+}
+
 //maybe make a bool function
-bool float2eeprom(float value, u_int16_t addr){  //from least to greatest byte values
- 
-  int byte0, byte1, byte2, byte3;
-  
-  int value2save;
+bool float2eeprom(float value, uint16_t addr){  //from least to greatest byte values
+  byte byte0, byte1, byte2, byte3;
+  int32_t value2save;
   bool matched;
 
-  value2save =  value*1000
+  value2save =  value*1000;  //convert to int to cicumvent data type issues
 
     byte0 = (value2save & 0xFF); //LSB //maybe make a bool function
     byte1 = ((value2save >> 8) & 0XFF);
     byte2 = ((value2save >> 16) & 0xFF);
     byte3 = ((value2save >> 24) & 0xFF);  //MSB
 
-    EEPROM.write(addr, byte_0);  //storing from least address to greatest
-    EEPROM.write(addr + 1, byte_1);
-    EEPROM.write(addr + 2, byte_2);
-    EEPROM.write(addr + 3, byte_3);
+    EEPROM.write(addr, byte0);  //storing from least address to greatest
+    EEPROM.write(addr + 1, byte1);
+    EEPROM.write(addr + 2, byte2);
+    EEPROM.write(addr + 3, byte3);
     if(value = eeprom2float(addr)){ //just == or =
       matched = true;
       Serial.printf("matched \n");
@@ -49,20 +54,20 @@ bool float2eeprom(float value, u_int16_t addr){  //from least to greatest byte v
         matched =  false;
         Serial.printf("not matched \n");
     }
+    return matched;
 }
-float eeprom2float(u_int16_t _addr){ 
-    int byte0, byte1, byte2, byte3; //ordered LSB TO MSB
+float eeprom2float(uint16_t _addr){ 
+    byte byte0, byte1, byte2, byte3; //ordered LSB TO MSB
     int value;
     float valueF;
 
-  byte0 = EEPROM.read(addr);  // values in specfic memory location
-  byte1 = EEPROM.read(_startingAddress+1);
-  byte2 = EEPROM.read(_startingAddress+2);
-  byte3 = EEPROM.read(_startingAddress+3);
+  byte0 = EEPROM.read(_addr);  // values in specfic memory location
+  byte1 = EEPROM.read(_addr+1);
+  byte2 = EEPROM.read(_addr+2);
+  byte3 = EEPROM.read(_addr+3);
 
   value =  (byte3 << 24) | (byte2 << 16) | (byte1 << 8) | (byte0);
-  valueF = value/1000.0
-  Serial.printf("Value %.2f \n " , value);
+  valueF = value/1000.0;
+  Serial.printf("Value %.2f \n " , valueF);
   return valueF;
 }
-
